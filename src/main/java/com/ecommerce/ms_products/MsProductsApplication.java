2426da8fa1228405744a6445119781e41732109e
guerreroeileen <eileenguerrerogomez@gmail.com>
2024-06-22 16:29:10 -0500

    first commit

---
+++ src/main/java/com/ecommerce/ms_products/MsProductsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ms_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductsApplication.class, args);
+	}
+
+}
